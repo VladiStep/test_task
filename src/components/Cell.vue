3edd45653c80008e85a674df9d8ba44f43d0f2ed
VladiStep <vlad2001.21@mail.ru>
2024-07-16 00:38:01 +0300

    Add cell images.

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,21 +1,40 @@
 <template>
- <div class="cell-cont">
+  <div class="cell-cont">
+    <img class="icon" :src="getImgUrl(item?.iconName)" />
 
- </div>
+    <div class="quantity-cont"></div>
+  </div>
 </template>
 
 <script setup lang="ts">
-  import { PropType } from 'vue';
-  import { IItem } from '../stores/inventory';
+  import { PropType } from "vue";
+  import { IItem } from "../stores/inventory";
 
   const props = defineProps({
     position: Number,
-    item: Object as PropType<IItem> 
+    item: Object as PropType<IItem>
   });
+
+  const getImgUrl = (name: string | undefined) => {
+    if (name === undefined) return "";
+
+    return require('../assets/' + name);
+  }
 </script>
 
 <style scoped lang="scss">
- .cell-cont {
+  .cell-cont {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
- }
+  .icon {
+    width: 54px;
+    height: 54px;
+  }
+
+  .quantity-cont {
+
+  }
 </style>
